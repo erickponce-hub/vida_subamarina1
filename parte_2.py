5dd974febe12fc5628bac5e25f0dda803870cead
erickponce-hub <erickalfredopr@isb.edu.mx>
2025-11-12 07:46:46 -0600

    Se sube parte 2 completa

--- parte_2.py
+++ parte_2.py
@@ -29,4 +29,50 @@ alga_img=pygame.transform.scale(alga_img,(40,40))
 
 algas=[]
 for i in range(3):
-    alga=alga_img.get_rect(topleft=(random.randint(0,WIDHT-40),random))
+    alga=alga_img.get_rect(topleft=(random.randint(0,WIDTH-40),random.randint(0,HEIGHT-40)))
+    algas.append({"img":alga_img, "rect": alga, "speed":random.randint(3,6)})
+    
+points=0
+
+
+#pez
+fish=pez_img.get_rect(topleft=(WIDTH//2,HEIGHT-60))
+fish_speed=7
+
+def draw_text(text,x,y,color=(255,255,255)):
+    img=font.render(text,True,color)
+    screen.blit(img,(x,y))
+    
+while True:
+    screen.blit(fondo,(0,0))
+    
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    Keys=pygame.key.get_pressed()
+    if Keys [pygame.K_LEFT]:fish.x-=fish_speed
+    if Keys [pygame.K_RIGHT]:fish.x+=fish_speed
+    if Keys [pygame.K_UP]:fish.y-=fish_speed
+    if Keys [pygame.K_DOWN]:fish.y+=fish_speed
+    
+    fish.clamp_ip(screen.get_rect())
+    screen.blit(pez_img,fish.topleft)
+    
+    for alga in algas:
+        alga["rect"].y+=alga["speed"]
+        screen.blit(alga["img"],alga["rect"].topleft)
+        
+        if alga["rect"].top>HEIGHT:
+            alga["rect"].topleft=(random.randint(0,WIDTH-40),random.randint(50,300))
+            
+        if fish.colliderect(alga["rect"]):
+            points+=1
+            bite_sound.play()
+            alga["rect"].topleft=(random.randint(0,WIDTH-40),random.randint(50,300))
+            
+    draw_text(f"Puntos: {points}", 10, 10)
+    
+    pygame.display.flip()
+    clock.tick(60)
